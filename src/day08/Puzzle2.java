25c8842cc8d27d4b8cc4a2b1af03ec1d1a101611
Joerg Pleumann <joerg.pleumann@gmail.com>
2022-02-18 16:02:16 +0100

    Optimizing day 8 part 2 (bitsets instead of strings)

--- src/day08/Puzzle2.java
+++ src/day08/Puzzle2.java
@@ -3,94 +3,92 @@ package day08;
 import java.util.ArrayList;
 
 /**
- *  Seven Segment Search part 2. Poor man's set processing based on strings.
+ *  Seven Segment Search part 2.
  */
 class Puzzle2 extends Puzzle1 {
-    
+
     /**
-     * Returns "s minus t".
+     * Converts a signal string into a bit pattern (a->1, b->2, c->4, ...).
      */
-    String diff(String s, String t) {
-        String result = "";
+    int toBits(String s) {
+        int i = 0;
         
         for (char c: s.toCharArray()) {
-            if (t.indexOf(c) == -1) {
-                result += c;
-            }
+            i = i | (1 << (c - 'a'));
         }
         
-        return result;
+        return i;
     }
 
     /**
-     * Returns "s equals t".
+     * Converts a bit pattern into a signal string (1->a, 2->b 4->c, ...).
      */
-    boolean same(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+    String toChars(int i) {
+        StringBuilder s = new StringBuilder();
         
-        for (char c: s.toCharArray()) {
-            if (t.indexOf(c) == -1) {
-                return false;
+        char c = 'a';
+        while (i != 0) {
+            if ((i & 1) == 1) {
+                s.append(c);
             }
+            c = (char)(c + 1);
+            i = i >> 1;
         }
         
-        return true;
+        return s.toString();
     }
 
     /**
      * Finds in a list of signal combinations the one that contains "length"
-     * signals, has all "include" signals and no "exlude" signal.
+     * signals and is a proper superset of "include" (i.e. contains it fully).
      */
-    String find(ArrayList<String> wires, int length, String include, String exclude) {
+    int find(ArrayList<Integer> wires, int length, int include) {
         for (int i = wires.size() - 1; i >= 0; i--) {
-            String s = wires.get(i);
+            int s = wires.get(i);
             
-            if (s.length() != length) continue;                
-            if (diff(include, s).length() != 0) continue;
-            if (diff(exclude, s).length() < exclude.length()) continue;
+            if (Integer.bitCount(s) != length) continue;                
+            if ((s & include) != include) continue;
             
             wires.remove(i);
-            System.out.println(s);
+            System.out.println(toChars(i));
             
             return s;
         }
         
-        return null;
+        throw new RuntimeException("Oops!");
     }
     
     /**
      * Solves one line of input.
      */
-    void analyze(ArrayList<String> wires, String[] numbers) {
-        String[] digits = new String[10];
+    void analyze(ArrayList<Integer> wires, int[] numbers) {
+        int[] digits = new int[10];
 
-        digits[1] = find(wires, 2, "", "");
-        digits[4] = find(wires, 4, "", "");
-        digits[7] = find(wires, 3, "", "");
-        digits[8] = find(wires, 7, "", "");
+        digits[1] = find(wires, 2, 0);
+        digits[4] = find(wires, 4, 0);
+        digits[7] = find(wires, 3, 0);
+        digits[8] = find(wires, 7, 0);
 
-        digits[3] = find(wires, 5, digits[1], "");
-        digits[9] = find(wires, 6, digits[4], "");
-        digits[0] = find(wires, 6, digits[1], "");
-        digits[6] = find(wires, 6, "", "");
+        digits[3] = find(wires, 5, digits[1]);
+        digits[9] = find(wires, 6, digits[4]);
+        digits[0] = find(wires, 6, digits[1]);
+        digits[6] = find(wires, 6, 0);
         
-        digits[2] = find(wires, 5, diff(digits[8], digits[6]), "");   
+        digits[2] = find(wires, 5, digits[8] ^ digits[6]);   
 
-        digits[5] = find(wires, 5, "", "");   
+        digits[5] = find(wires, 5, 0);   
         
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " -> " + digits[i]);
+            System.out.println(i + " -> " + toChars(digits[i]));
         }
         
         int n = 0;
         
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < 10; j++) {
-                if (same(numbers[i], digits[j])) {
+                if (numbers[i] == digits[j]) {
                     n = n * 10 + j;
-                    System.out.println(numbers[i] + " = " + j);
+                    System.out.println(toChars(numbers[i]) + " = " + j);
                 }
             }
         }
@@ -99,18 +97,24 @@ class Puzzle2 extends Puzzle1 {
         
         count += n;
     }
-     
+    
     @Override
     void processLine(String line) {
         String[] a = line.split("\\s\\|\\s"); // Separate left/right part
         String[] b = a[1].trim().split(" ");  // Separate the four digits
         String[] c = a[0].trim().split(" ");  // Separate signal patterns
                 
-        ArrayList<String> wires = new ArrayList();
+        ArrayList<Integer> wires = new ArrayList();
         for (String s: c) {
-            wires.add(s);
+            wires.add(toBits(s));
         }
-        analyze(wires, b);
+        
+        int[] digits = new int[4];
+        for (int i = 0; i < 4; i++) {
+            digits[i] = toBits(b[i]);
+        }
+        
+        analyze(wires, digits);
 
         System.out.println();
     }
